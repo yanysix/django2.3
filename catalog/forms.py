f02f5d551457869c2e7f8815d7f0144d883bf182
yana <taratynova.yana@bk.ru>
2025-03-22 04:38:16 +0700

    Форма для изменения статуса на вып

--- catalog/forms.py
+++ catalog/forms.py
@@ -60,7 +60,14 @@ class RequestCreateForm(forms.ModelForm):
         model = Request
         fields = ('name', 'description', 'category', 'image', )
 
-        class RequestWorkStatusChangeForm(forms.ModelForm):
+class RequestDoneStatusChangeForm(forms.ModelForm):
+    image_done = forms.ImageField(label='Готовое изображение', required=True)
+
+    class Meta:
+        model = Request
+        fields = ('image_done',)
+
+class RequestWorkStatusChangeForm(forms.ModelForm):
             comment = forms.CharField(label='Комментарий', widget=forms.TextInput, required=True)
 
             class Meta:
